06d61cbf0eefc0e52a6c8a5abb71fed49015e66d
Mike Mulhausen <michael.mulhausen@keylimetie.com>
2018-07-19 16:18:27 -0500

    email script updated to send all required information in message

--- mail.php
+++ mail.php
@@ -5,16 +5,20 @@
 	try {
 		$appid = AppIdentityService::getApplicationId();
 		
-		$firstname = $_POST["name"];
+		$name = $_POST["name"];
     	$email= $_POST["email"];
     	$text= $_POST["message"];
    	 	$subject = $_POST["subject"];
+        
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$text\n";
 		
 	    $message = new Message();
 	    $message->setSender('noreply@' . $appid . '.appspotmail.com');
 	    $message->addTo('michael.mulhausen@gmail.com');
 	    $message->setSubject($subject);
-	    $message->setTextBody($text);
+	    $message->setTextBody($email_content);
 	    $message->send();
 	    http_response_code(200);
         echo "Thank You! Your message has been sent.";
